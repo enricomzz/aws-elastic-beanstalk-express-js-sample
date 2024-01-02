7c3122af8f77d3f09ff5b8d250bb563f38bbd640
Enrico Mazzarella <enrico.mazzarella@it-present.com>
2024-01-02 10:53:43 +0100

    updated the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Enrico, This a 2nd version!'));
+app.get('/', (req, res) => res.send('Hello Enrico, This a 2024 version!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
