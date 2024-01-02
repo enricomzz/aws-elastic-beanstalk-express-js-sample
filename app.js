c0ae88069fa25930ed673cb7f08daff28e37bdf7
Enrico Mazzarella <enrico.mazzarella@it-present.com>
2024-01-02 11:54:56 +0100

    added comments for manual approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Enrico, This a 2024 version!'));
+app.get('/', (req, res) => res.send('Hello Enrico, This a 2024 version, revised for approval!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
